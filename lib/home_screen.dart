2453ce7438ad791547573bc9512f124c2851913b
Mohamed Essam <77198018+MohamedEssam9009@users.noreply.github.com>
2024-04-23 14:48:38 +0200

    finish the app

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -7,9 +7,45 @@ class HomeScreen extends StatefulWidget {
   State<HomeScreen> createState() => _HomeScreenState();
 }
 
-class _HomeScreenState extends State<HomeScreen> {
+class _HomeScreenState extends State<HomeScreen>
+    with SingleTickerProviderStateMixin {
+  late AnimationController animationController;
+
+  @override
+  void initState() {
+    super.initState();
+    animationController = AnimationController(
+      vsync: this,
+      duration: const Duration(seconds: 1),
+    );
+  }
+
+  bool isDone = false;
+  void switchIcon() {
+    if (isDone == false) {
+      animationController.forward();
+      isDone = true;
+    } else {
+      animationController.reverse();
+      isDone = false;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.pink,
+      body: Center(
+        child: GestureDetector(
+          onTap: switchIcon,
+          child: AnimatedIcon(
+            color: Colors.white,
+            size: 150.0,
+            icon: AnimatedIcons.play_pause,
+            progress: animationController,
+          ),
+        ),
+      ),
+    );
   }
 }
